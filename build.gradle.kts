7c245b39b4d6833c0822ff9c3d38f85dfd843267
Alexandre Dutra <adutra@users.noreply.github.com>
2025-02-22 12:13:48 +0100

    Add Gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.net.URI
+import org.nosphere.apache.rat.RatTask
+
+buildscript { repositories { maven { url = java.net.URI("https://plugins.gradle.org/m2/") } } }
+
+plugins {
+  id("idea")
+  id("eclipse")
+  id("authmgr-root")
+  alias(libs.plugins.rat)
+}
+
+val projectName = rootProject.file("ide-name.txt").readText().trim()
+val ideName = "$projectName ${rootProject.version.toString().replace("^([0-9.]+).*", "\\1")}"
+
+if (System.getProperty("idea.sync.active").toBoolean()) {
+  // There's no proper way to set the name of the IDEA project (when "just importing" or
+  // syncing the Gradle project)
+  val ideaDir = rootProject.layout.projectDirectory.dir(".idea")
+  ideaDir.asFile.mkdirs()
+  ideaDir.file(".name").asFile.writeText(ideName)
+}
+
+eclipse { project { name = ideName } }
+
+tasks.withType(JavaCompile::class.java).configureEach { options.release = 11 }
+
+tasks.named<RatTask>("rat").configure {
+  // These are Gradle file pattern syntax
+  excludes.add("**/build/**")
+
+  excludes.add("LICENSE")
+  excludes.add("NOTICE")
+
+  excludes.add("ide-name.txt")
+  excludes.add("version.txt")
+  excludes.add(".git")
+  excludes.add(".gradle")
+  excludes.add(".idea")
+  excludes.add(".java-version")
+  excludes.add("**/.keep")
+
+  excludes.add("gradle/wrapper/gradle-wrapper*.jar*")
+
+  excludes.add("**/kotlin-compiler*")
+  excludes.add("**/build-logic/.kotlin/**")
+}
